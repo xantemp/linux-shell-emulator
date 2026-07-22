463a82b3dd4db05e561d2bcc7cc2a6cf01b105b7
xantemp <Xantemp@hotmail.com>
2026-07-22 15:50:05 -0400

    Add React main entry point

---
+++ frontend/src/main.jsx
@@ -0,0 +1,1 @@
+import React from 'react'\nimport ReactDOM from 'react-dom/client'\nimport App from './App.jsx'\nimport './index.css'\n\nReactDOM.createRoot(document.getElementById('root')).render(\n  <React.StrictMode>\n    <App />\n  </React.StrictMode>,\n)\n
